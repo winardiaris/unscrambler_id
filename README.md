800e1900767d5199e9f26b96e8b5707c60bc175b
winardiaris <aris.winardi@finaccel.co>
2023-04-27 14:29:33 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Unscrambler ID
+Resolve unscramble word in Bahasa Indonesia
+
+
+## Requirements
+- python
+- pip
+
+## How to install
+```bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/winardiaris/unscrambler_id/master/install.sh)"
+```
--- install.sh
+++ install.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
-ROOT_DIR=$HOME/.unscrambler_id
+UNSCRAMBLER_DIR=$HOME/.unscrambler_id
 
-git clone --recursive https://github.com/winardiaris/unscrambler_id.git "$ROOT_DIR"
-cd  "$ROOT_DIR"|| exit
-git config --global --add safe.directory "$ROOT_DIR"
+if ! [ -x "$(command -v git)" ]; then
+    echo 'Error: git is not installed.' >&2
+    exit 1
+fi
+
+if ! [ -x "$(command -v python)" ]; then
+    echo 'Error: python is not installed.' >&2
+    exit 1
+fi
+
+if ! [ -x "$(command -v pip)" ]; then
+    echo 'Error: pip is not installed.' >&2
+    exit 1
+fi
+
+git clone --recurse-submodules https://github.com/winardiaris/unscrambler_id.git "$UNSCRAMBLER_DIR"
+git config --global --add safe.directory "$UNSCRAMBLER_DIR"
+cd "$UNSCRAMBLER_DIR" || exit
+pip install -r requirement.txt
+
+if [ "$SHELL" == '/usr/bin/zsh' ]; then
+    echo "UNSCRAMBLER_DIR=$HOME/.unscrambler_id" >>~/.zshrc
+    echo "export PATH=\"\$UNSCRAMBLER_DIR:\$PATH\"" >>~/.zshrc
+elif [ "$SHELL" == '/bin/bash' ]; then
+    echo "UNSCRAMBLER_DIR=$HOME/.unscrambler_id" >>~/.bashrc
+    echo "export PATH=\"\$UNSCRAMBLER_DIR:\$PATH\"" >>~/.bashrc
+else
+    echo "Please add ${UNSCRAMBLER_DIR} to your PATH"
+fi
